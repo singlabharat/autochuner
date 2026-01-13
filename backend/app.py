b8f855d9c742d08ffaaf6839bd1f30419acb10c2
Bharat Singla <58552958+singlabharat@users.noreply.github.com>
2026-01-13 12:40:11 +1100

    upd readme

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import base64
